606ff6a5de5ae8d2a2a2828beb2e832da36b05fb
Ana Castaneda <anapaulina011@gmail.com>
2024-11-19 10:52:33 -0500

    started Lab 5, testsnowball() works

---
+++ Lab05/lab05.py
@@ -0,0 +1,292 @@
+#!/usr/bin/env python3
+
+'''
+Draft code for Lab 5: SNOWBALL EARTH!!!
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.style.use('fivethirtyeight')
+
+# Some constants:
+radearth = 6357000.  # Earth radius in meters.
+mxdlyr = 50.         # depth of mixed layer (m)
+sigma = 5.67e-8      # Steffan-Boltzman constant
+C = 4186#4.2e6            # Heat capacity of water
+rho = 1020           # Density of sea-water (kg/m^3)
+epsilon = 1          # Emissivity of blackbody
+albedo_ice = 0.6
+albedo_gnd = 0.3
+
+
+def gen_grid(nbins=18):
+    '''
+    Generate a grid from 0 to 180 lat (where 0 is south pole, 180 is north)
+    where each returned point represents the cell center.
+
+    Parameters
+    ----------
+    nbins : int, defaults to 18
+        Set the number of latitude bins.
+
+    Returns
+    -------
+    dlat : float
+        Grid spacing in degrees.
+    lats : Numpy array
+        Array of cell center latitudes.
+    '''
+
+    dlat = 180 / nbins  # Latitude spacing.
+    lats = np.arange(0, 180, dlat) + dlat/2.
+
+    # Alternative way to obtain grid:
+    # lats = np.linspace(dlat/2., 180-dlat/2, nbins)
+
+    return dlat, lats
+
+
+def temp_warm(lats_in):
+    '''
+    Create a temperature profile for modern day "warm" earth.
+
+    Parameters
+    ----------
+    lats_in : Numpy array
+        Array of latitudes in degrees where temperature is required
+
+    Returns
+    -------
+    temp : Numpy array
+        Temperature in Celcius.
+    '''
+
+    # Get base grid:
+    dlat, lats = gen_grid()
+
+    # Set initial temperature curve:
+    T_warm = np.array([-47, -19, -11, 1, 9, 14, 19, 23, 25, 25,
+                       23, 19, 14, 9, 1, -11, -19, -47])
+    coeffs = np.polyfit(lats, T_warm, 2)
+
+    # Now, return fitting:
+    temp = coeffs[2] + coeffs[1]*lats_in + coeffs[0] * lats_in**2
+
+    return temp
+
+
+def insolation(lats, S0=1370):
+    '''
+    Given a solar constant (`S0`), calculate average annual, longitude-averaged
+    insolation values as a function of latitude.
+    Insolation is returned at position `lats` in units of W/m^2.
+
+    Parameters
+    ----------
+    S0 : float
+        Solar constant (1370 for typical Earth conditions.)
+    lats : Numpy array
+        Latitudes to output insolation. Following the grid standards set in
+        the diffusion program, polar angle is defined from the south pole.
+        In other words, 0 is the south pole, 180 the north.
+
+    Returns
+    -------
+    insolation : numpy array
+        Insolation returned over the input latitudes.
+    '''
+
+    # Constants:
+    max_tilt = 23.5   # tilt of earth in degrees
+
+    # Create an array to hold insolation:
+    insolation = np.zeros(lats.size)
+
+    #  Daily rotation of earth reduces solar constant by distributing the sun
+    #  energy all along a zonal band
+    dlong = 0.01  # Use 1/100 of a degree in summing over latitudes
+    angle = np.cos(np.pi/180. * np.arange(0, 360, dlong))
+    angle[angle < 0] = 0
+    total_solar = S0 * angle.sum()
+    S0_avg = total_solar / (360/dlong)
+
+    # Accumulate normalized insolation through a year.
+    # Start with the spin axis tilt for every day in 1 year:
+    tilt = [max_tilt * np.cos(2.0*np.pi*day/365) for day in range(365)]
+
+    # Apply to each latitude zone:
+    for i, lat in enumerate(lats):
+        # Get solar zenith; do not let it go past 180. Convert to latitude.
+        zen = lat - 90. + tilt
+        zen[zen > 90] = 90
+        # Use zenith angle to calculate insolation as function of latitude.
+        insolation[i] = S0_avg * np.sum(np.cos(np.pi/180. * zen)) / 365.
+
+    # Average over entire year; multiply by S0 amplitude:
+    insolation = S0_avg * insolation / 365
+
+    return insolation
+
+def snowball_earth(nbins=18, dt=1., tstop=10000, lam=100., spherecorr=True,
+                   insol=True, debug=False):
+    '''
+    Perform snowball earth simulation.
+
+    Parameters
+    ----------
+    nbins : int, defaults to 18
+        Number of latitude bins.
+    dt : float, defaults to 1
+        Timestep in units of years
+    tstop : float, defaults to 10,000
+        Stop time in years
+    lam : float, defaults to 100
+        Diffusion coefficient of ocean in m^2/s
+    spherecorr : bool, defaults to True
+        Use the spherical coordinate correction term. This should always be
+        true except for testing purposes.
+    debug : bool, defaults to False
+        Turn  on or off debug print statements.
+
+    Returns
+    -------
+    lats : Numpy array
+        Latitude grid in degrees where 0 is the south pole.
+    Temp : Numpy array
+        Final temperature as a function of latitude.
+    '''
+    # Get time step in seconds:
+    dt_sec = 365 * 24 * 3600 * dt  # Years to seconds.
+
+    # Generate grid:
+    dlat, lats = gen_grid(nbins)
+
+    # Get grid spacing in meters.
+    dy = radearth * np.pi * dlat / 180.
+
+    # Create initial condition:
+    Temp = temp_warm(lats)
+    if debug:
+        print('Initial temp = ', Temp)
+
+    # Get number of timesteps:
+    nstep = int(tstop / dt)
+
+    # Debug for problem initialization
+    if debug:
+        print("DEBUG MODE!")
+        print(f"Function called for nbins={nbins}, dt={dt}, tstop={tstop}")
+        print(f"This results in nstep={nstep} time step")
+        print(f"dlat={dlat} (deg); dy = {dy} (m)")
+        print("Resulting Lat Grid:")
+        print(lats)
+
+    # Build A matrix:
+    if debug:
+        print('Building A matrix...')
+    A = np.identity(nbins) * -2  # Set diagonal elements to -2
+    A[np.arange(nbins-1), np.arange(nbins-1)+1] = 1  # Set off-diag elements
+    A[np.arange(nbins-1)+1, np.arange(nbins-1)] = 1  # Set off-diag elements
+    # Set boundary conditions:
+    A[0, 1], A[-1, -2] = 2, 2
+
+    # Build "B" matrix for applying spherical correction:
+    B = np.zeros((nbins, nbins))
+    B[np.arange(nbins-1), np.arange(nbins-1)+1] = 1  # Set off-diag elements
+    B[np.arange(nbins-1)+1, np.arange(nbins-1)] = -1  # Set off-diag elements
+    # Set boundary conditions:
+    B[0, :], B[-1, :] = 0, 0
+
+    # Set the surface area of the "side" of each latitude ring at bin center.
+    Axz = np.pi * ((radearth+50.0)**2 - radearth**2) * np.sin(np.pi/180.*lats)
+    dAxz = np.matmul(B, Axz) / (Axz * 4 * dy**2)
+
+    if debug:
+        print('A = ', A)
+    # Set units of A derp
+    A /= dy**2
+
+    # Get our "L" matrix:
+    L = np.identity(nbins) - dt_sec * lam * A
+    L_inv = np.linalg.inv(L)
+
+    if debug:
+        print('Time integrating...')
+    for i in range(nstep):
+        # Add spherical correction term:
+        if spherecorr:
+            Temp += dt_sec * lam * dAxz * np.matmul(B, Temp)
+            if debug:
+                print(f'Temp after sphere corr: {Temp}')
+        if insol:
+            # Update albedo based on conditions:
+            # loc_ice = Temp <= -10
+            # albedo = np.zeros_like(Temp)
+            # albedo[loc_ice] = albedo_ice
+            # albedo[~loc_ice] = albedo_gnd
+            ins = (dt_sec/(rho*C*mxdlyr)) * (insolation(lats)*(1-0.3)-epsilon*sigma*(Temp+273.15)**4)
+            Temp += ins
+            if debug:
+                print(f'ins term: {ins}')
+                print(f'Temp after insol: {Temp}')
+
+        Temp = np.matmul(L_inv, Temp)
+
+    return lats, Temp
+
+
+def test_snowball(tstop=10000):
+    '''
+    Reproduce example plot in lecture/handout.
+
+    Using our DEFAULT values (grid size, diffusion, etc.) and a warm-Earth
+    initial condition, plot:
+        - Initial condition
+        - Plot simple diffusion only
+        - Plot simple diffusion + spherical correction
+        - Plot simple diff + sphere corr + insolation
+    '''
+    nbins = 18
+
+    # Generate very simple grid
+    # Generate grid:
+    dlat, lats = gen_grid(nbins)
+
+    # Create initial condition:
+    initial = temp_warm(lats)
+
+    # Get simple diffusion solution:
+    lats, t_diff = snowball_earth(tstop=tstop, spherecorr=False, insol=False)
+
+    # Get diffusion + spherical correction:
+    lats, t_sphe = snowball_earth(tstop=tstop, insol=False)
+    print(t_sphe[0])
+
+    # Get diff + spher corr + insolation
+    lats, t_ins =  snowball_earth(tstop=tstop)
+
+    # Create figure and plot!
+    fig, ax = plt.subplots(1, 1, figsize=(8, 6))
+
+    ax.plot(lats, initial, label='Warm Earth Init. Cond.')
+    ax.plot(lats, t_diff, label='Simple Diffusion')
+    ax.plot(lats, t_sphe, label='Diffusion + Sphere. Corr.')
+    ax.plot(lats, t_ins, label='Diff + Spherec Corr. + Insolation')
+
+    ax.set_xlabel('Latitude (0=South Pole)')
+    ax.set_ylabel('Temperature ($^{\circ} C$)')
+
+    ax.legend(loc='best')
+
+    fig.tight_layout()
+
+#lats, t_ins =  snowball_earth(tstop=3)
+
+test_snowball()
+
+# dlat, lats = gen_grid()
+# print(insolation(lats))
+# print(temp_warm(lats))
+
+# snowball_earth(tstop=3, debug=True)
